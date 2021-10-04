692de2788cb1fec7725e02559e8fab94f7882b2e
brickke <gebiertas.a@gmail.com>
2021-10-04 16:56:50 +0800

    Replace a new database.

--- db_primary.sql
+++ db_primary.sql
@@ -1,13 +1,13 @@
 -- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: db_primary
+-- Host: localhost    Database: db_primary
 -- ------------------------------------------------------
 -- Server version	8.0.26
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -39,6 +39,30 @@ LOCK TABLES `grade1` WRITE;
 INSERT INTO `grade1` VALUES (1,'mary','caranzo','macopa'),(2,'kate','sanchez','ponkan'),(3,'john','bautista','orange'),(4,'ann','ladres','orange'),(5,'zen','ladres','macopa');
 /*!40000 ALTER TABLE `grade1` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `grade2`
+--
+
+DROP TABLE IF EXISTS `grade2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `grade2` (
+  `id` int NOT NULL,
+  `firstname` varchar(30) DEFAULT NULL,
+  `lastname` varchar(30) DEFAULT NULL,
+  `section` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `grade2`
+--
+
+LOCK TABLES `grade2` WRITE;
+/*!40000 ALTER TABLE `grade2` DISABLE KEYS */;
+/*!40000 ALTER TABLE `grade2` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -49,4 +73,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-04 16:20:24
+-- Dump completed on 2021-10-04 16:54:46
